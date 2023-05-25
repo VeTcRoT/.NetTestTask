edb684266f4f3dd14e999c797b6e81b3d1fc5191
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-25 14:57:28 +0300

    Added DbContext and Employee entity

---
+++ TestTask/TestTask/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestTask.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TestTask.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
