09844296693acc285c6b433b56720afbe81aa97f
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-25 17:34:15 +0300

    Implemented listing of employees on page

--- TestTask/TestTask/Controllers/HomeController.cs
+++ TestTask/TestTask/Controllers/HomeController.cs
@@ -1,32 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using TestTask.Models;
 
 namespace TestTask.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeRepository _employeeRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IEmployeeRepository employeeRepository)
         {
-            _logger = logger;
+            _employeeRepository = employeeRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index() 
         {
-            return View();
-        }
+            var employees = await _employeeRepository.GetAllAsync();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(employees);
         }
     }
 }
