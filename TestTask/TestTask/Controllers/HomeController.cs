05d82c82f2bf55928c934666355206df5ee5db82
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-25 18:57:24 +0300

    Added inline employee updating

--- TestTask/TestTask/Controllers/HomeController.cs
+++ TestTask/TestTask/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 using TestTask.Models;
 
@@ -47,6 +48,42 @@ namespace TestTask.Controllers
                         }
                     }
                 }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, string field, string value)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            var property = typeof(Employee).GetProperty(field);
+
+            if (property != null)
+            {
+                if (property.PropertyType == typeof(bool))
+                {
+                    property.SetValue(employee, bool.Parse(value));
+                }
+                else if (property.PropertyType == typeof(decimal))
+                {
+                    property.SetValue(employee, decimal.Parse(value));
+                }
+                else if (property.PropertyType == typeof(DateTime))
+                {
+                    property.SetValue(employee, DateTime.Parse(value));
+                }
+                else
+                {
+                    property.SetValue(employee, value);
+                }
+
+                await _employeeRepository.UpdateAsync(employee);
             }
 
             return RedirectToAction("Index");
